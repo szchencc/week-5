30d83636e4f049bec570f37fc6ad796ba0fc31ee
szchencc <xc2324@columbia.edu>
2015-10-28 07:02:21 +0800

    finish
    ~

--- static/script.js
+++ static/script.js
@@ -31,6 +31,8 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 var svg = d3.select(map.getPanes().overlayPane).append("svg");
 var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+var svg_overlay = d3.select(map.getPanes().overlayPane).append("svg");
+var g_overlay = svg_overlay.append("g").attr("class", "leaflet-zoom-hide");
 
 function projectPoint(lat, lng) {
 	return map.latLngToLayerPoint(new L.LatLng(lat, lng));
@@ -52,8 +54,11 @@ function updateData(){
 	var lng1 = mapBounds["_southWest"]["lng"];
 	var lng2 = mapBounds["_northEast"]["lng"];
 
-	request = "/getData?lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2
+	var cell_size = 25;
+	var w = window.innerWidth;
+	var h = window.innerHeight;
 
+	request = "/getData?lat1=" + lat1 + "&lat2=" + lat2 + "&lng1=" + lng1 + "&lng2=" + lng2 + "&w=" + w + "&h=" + h + "&cell_size=" + cell_size
 	console.log(request);
 
   	d3.json(request, function(data) {
@@ -76,10 +81,25 @@ function updateData(){
 				tooltip.style("visibility", "hidden");
 			})
 		;
+		
+		var topleft = projectPoint(lat2, lng1);
 
+		svg_overlay.attr("width", w)
+    		.attr("height", h)
+    		.style("left", topleft.x + "px")
+    		.style("top", topleft.y + "px");
+		var rectangles = g_overlay.selectAll("rect").data(data.analysis);
+		rectangles.enter().append("rect");
+		rectangles
+		    .attr("x", function(d) { return d.x; })
+		    .attr("y", function(d) { return d.y; })
+		    .attr("width", function(d) { return d.width; })
+		    .attr("height", function(d) { return d.height; })
+		    .attr("fill-opacity", ".2")
+		    .attr("fill", function(d) { return "hsl(0, " + Math.floor(d.value*100) + "%, 50%)"; });
 		// function to update the data
 		function update() {
-
+			g_overlay.selectAll("rect").remove()
 			// get bounding box of data
 		    var bounds = path.bounds(data),
 		        topLeft = bounds[0],
